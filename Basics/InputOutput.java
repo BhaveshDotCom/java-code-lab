bce541d7e94931e609dc069a145de1845e0d9392
BhaveshDotCom <mail.bhavesh.in@gmail.com>
2026-06-29 19:01:31 +0530

    add IO

---
+++ Basics/InputOutput.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class InputOutput {
+
+    public static void main(String[] args) {
+        var input = IO.readln("Enter: ");
+        IO.println(input);
+    }
+    
+}
--- DataStructure/JavaCollectionFramework/CollectionsMethods.java
+++ DataStructure/JavaCollectionFramework/CollectionsMethods.java
@@ -6,12 +6,18 @@ public class CollectionsMethods {
         Collection<Integer> col = new ArrayList<>();
         col.add(1);
         System.out.println(col.isEmpty());
+
         ArrayList<Integer> arr = new ArrayList<>(List.of(1,2,3,4,5));
+
         Iterator<Integer> it = arr.iterator();
+
         while (it.hasNext()) {
             System.out.println(it.next());
         }
+        
         IO.println(Math.max(11, 10));
+
+        
         
     }
 
